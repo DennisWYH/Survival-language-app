80d200f0629ce886534e2e9d6406d45a97b844d5
wangy <wangy@transip.nl>
2024-03-17 10:38:55 +0100

    add git ignore file

--- card/admin.py
+++ card/admin.py
@@ -7,6 +7,14 @@ from django.utils import timezone
 class CardAdmin(admin.ModelAdmin):
     fields = ["image", "text", "lan", "creation_date", "modification_date", "upload_by_userName"]
     list_display = ["image", "lan", "text", "upload_by_userName", "modification_date", "creation_date"]
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        # Get the object instance
+        obj = Card.objects.get(pk=object_id)
+        # Pass the instance to the template context
+        extra_context = extra_context or {}
+        extra_context['instance'] = obj
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)      
+       
     def get_form(self, request, obj=None, **kwargs):
             form = super().get_form(request, obj, **kwargs)
             if obj is None:  # Only fill the field for new instances
