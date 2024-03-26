fa04af4a721fc5a7dbc613155056ba25181876fd
wangy <wangy@transip.nl>
2024-03-26 16:17:42 +0100

    add user app

--- languageApp/urls.py
+++ languageApp/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from user.views import user_profile
 
 urlpatterns = [
     path("", include("card.urls")),
-    path("user/<str:username>", views.user_profile, name="user_profile"),
+    path("user/", user_profile),
     path("card/", include("card.urls")),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
