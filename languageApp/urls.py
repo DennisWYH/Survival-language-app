7a564c18d1c250f091dd3f8967485d79c883aa7f
wangy <wangy@transip.nl>
2024-03-27 11:12:56 +0100

    improve styleing

--- languageApp/urls.py
+++ languageApp/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 from .views import login_view
 
 urlpatterns = [
-    path("login/", login_view, name="login"),
     path("", include("card.urls")),
+    path("login/", login_view, name="login"),
     path("user/", include('user.urls')),
     path("card/", include("card.urls")),
     path('admin/', admin.site.urls),
