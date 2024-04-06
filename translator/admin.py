975ffce771dd6a4cdee0943ae4460e4e70a73e9c
wangy <wangy@transip.nl>
2024-04-06 15:29:28 +0200

    add an image preview on the chagne view of admin page of translator model

--- translator/admin.py
+++ translator/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import TextTranslator
 
-admin.site.register(TextTranslator)
+class TextTranslatorAdmin(admin.ModelAdmin):
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        # Get the object instance
+        obj = TextTranslator.objects.get(pk=object_id)
+        # Pass the instance to the template context
+        extra_context = extra_context or {}
+        extra_context["instance"] = obj
+        return super().change_view(
+            request, object_id, form_url, extra_context=extra_context
+        )
+
+admin.site.register(TextTranslator, TextTranslatorAdmin)
