1383bcb56411181c7507a09185c7eec1745d4830
wangy <wangy@transip.nl>
2024-04-07 13:38:38 +0200

    bug fix

--- languageApp/settings.py
+++ languageApp/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # for files that doesn't belong to specific app
 STATICFILES_DIRS = [
