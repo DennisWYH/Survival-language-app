d06e51468e85852b90f60bdea8be91640d1e6515
wangy <wangy@transip.nl>
2024-04-13 15:52:39 +0200

    bug fix

--- card/management/commands/remove_dangling_images_prod.py
+++ card/management/commands/remove_dangling_images_prod.py
@@ -11,6 +11,7 @@ class Command(BaseCommand):
         # Get all original_image and png_image filenames
         original_image_names = set(Card.objects.exclude(original_image='').values_list('original_image', flat=True))
         png_image_names = set(Card.objects.exclude(png_image='').values_list('png_image', flat=True))
+        print("---- collection of original image db names", original_image_names)
 
         # Remove directory path from filenames
         original_image_names = {os.path.basename(name) for name in original_image_names}
@@ -28,8 +29,10 @@ class Command(BaseCommand):
         # Delete images in card directory that do not belong to any Card object
         for file in all_files:
             filename = os.path.basename(file.key)
-            if filename not in original_image_names and filename not in png_image_names:
-                s3Client.delete_object(Bucket=bucket_name, Key=file.key)
+            print("bucket file name", filename)
+            if filename not in original_image_names:
+                print("bucket file name not in db", filename, "removing")
+                s3Client.delete_object(Bucket="media", Key=file.key)
 
         # This works to delete one object
         # s3Client = boto3.client('s3',aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),endpoint_url=os.getenv('AWS_S3_ENDPOINT_URL'))
